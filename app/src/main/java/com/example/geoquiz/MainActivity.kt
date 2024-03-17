e3f015201e954f32d3c13c51767a629bddcac1c6
Nikola <nikolatabas1@gmail.com>
2024-03-17 17:36:52 +0100

    add top bar

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.geoquiz
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,6 +9,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,7 +18,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -29,6 +34,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -48,12 +54,43 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //FrontPage(name = "Geo Quiz")
-                    Game()
+                    GeoQuizApp()
+                    //Game()
                 }
             }
         }
     }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun GeoQuizApp() {
+    Scaffold(topBar = { GeoQuizTopAppBar() }) {
+        FrontPage(name = "Geo quiz")
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun GeoQuizTopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    painter = painterResource(id = R.drawable.logo),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(dimensionResource(id = R.dimen.image_size))
+                        .padding(dimensionResource(id = R.dimen.padding_small))
+                )
+                Text(
+                    text = "GeoQuiz",
+                    style = MaterialTheme.typography.displayLarge
+                )
+            }
+        },
+        modifier = modifier
+    )
 }
 
 @Composable
@@ -71,45 +108,34 @@ fun FrontPage(name: String, modifier: Modifier = Modifier) {
             contentScale = ContentScale.Crop,
             modifier = Modifier.fillMaxSize()
         )
-        // Column to hold the text
         Column(
-            verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
             modifier = Modifier.fillMaxHeight()
         ) {
-            Text(
-                text = name,
-                fontSize = 50.sp,
-                modifier = Modifier.padding(20.dp)
+            TextField(
+                value = nickNameInput,
+                onValueChange = { nickNameInput = it },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Default
+                ),
+                label = { Text("Nickname") },
+                modifier = modifier
             )
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center,
-                modifier = Modifier.fillMaxHeight()
+            Spacer(modifier = Modifier.padding(20.dp))
+            Button(
+                onClick = {
+                    // Start new game logic
+                },
+                enabled = nickNameInput.isNotBlank()
             ) {
-                TextField(
-                    value = nickNameInput,
-                    onValueChange = { nickNameInput=it },
-                    singleLine = true,
-                    keyboardOptions = KeyboardOptions.Default.copy(
-                        keyboardType = KeyboardType.Text,
-                        imeAction = ImeAction.Default),
-                    label = { Text("Nickname") },
-                    modifier = modifier
-                )
-                Spacer(modifier = Modifier.padding(20.dp))
-                Button(
-                    onClick = {
-                        // Start new game logic
-                    },
-                    enabled= nickNameInput.isNotBlank()
-                ) {
-                    Text(text = "Start new game")
-                }
-                Spacer(modifier = Modifier.padding(20.dp))
-                Button(onClick = { /*TODO*/ }) {
-                    Text(text = "High scores")
-                }
+                Text(text = "Start new game")
+            }
+            Spacer(modifier = Modifier.padding(20.dp))
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "High scores")
             }
         }
         Column(
