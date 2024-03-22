4eeb24361513889581f4d324a102398bc1562e5a
Nikola <nikolatabas1@gmail.com>
2024-03-22 18:08:54 +0100

    fix warnings

--- app/src/main/java/com/example/geoquiz/ui/GeoQuizViewModel.kt
+++ app/src/main/java/com/example/geoquiz/ui/GeoQuizViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.geoquiz.ui
 
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
