b43d3f2a0fcb331fbfdb3c1693b8106b447f34b0
Nikola <nikolatabas1@gmail.com>
2024-03-25 19:44:08 +0100

    fix warnings

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,34 +1,13 @@
 package com.example.geoquiz
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.geoquiz.ui.GeoQuizViewModel
 import com.example.geoquiz.ui.theme.GeoQuizTheme
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.geoquiz.ui.ConnectGame
-import com.example.geoquiz.ui.FlagGame
-import com.example.geoquiz.ui.GeoQuizApp
 
 
 class MainActivity : ComponentActivity() {
