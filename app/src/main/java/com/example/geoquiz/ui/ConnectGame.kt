0e160d41953d15062de2c4337132f16af7ba3335
Nikola <nikolatabas1@gmail.com>
2024-04-03 21:14:51 +0200

    fix warnings

--- app/src/main/java/com/example/geoquiz/ui/ConnectGame.kt
+++ app/src/main/java/com/example/geoquiz/ui/ConnectGame.kt
@@ -1,14 +1,12 @@
 package com.example.geoquiz.ui
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.AlertDialog
@@ -48,7 +46,7 @@ fun ConnectGame(
     Column(
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize()) {
+        modifier = modifier.fillMaxSize()) {
         Text(
             text = stringResource(id = R.string.connect_capital),
             modifier = Modifier.padding(
